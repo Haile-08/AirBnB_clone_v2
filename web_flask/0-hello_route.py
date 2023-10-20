340c9cb4be114a37e1373d9c0d4cf257c82f9104
Haile-08 <Haile4cmd@gmail.com>
2023-10-20 12:56:42 +0000

    Add documantaion to the code and change host

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+start a flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello():
-	return "Hello HBNB!"
+def index():
+	"""return Hello HBNB!"""
+	return 'Hello HBNB!'
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0', port='5000')
